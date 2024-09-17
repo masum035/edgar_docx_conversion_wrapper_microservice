028ab1d0d9b7f6a818ab35f37717c86201633f5e
Masum <abdullah.almasum@wsd.com>
2024-09-17 10:56:31 +0600

    correlation-id added in each requests with interceptors

--- src/main/java/com/wsd/edgardocswrapper/config/ApiConfig.java
+++ src/main/java/com/wsd/edgardocswrapper/config/ApiConfig.java
@@ -1,11 +1,21 @@
 package com.wsd.edgardocswrapper.config;
 
+import com.wsd.edgardocswrapper.interceptors.CorrelationIdInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApiConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private CorrelationIdInterceptor correlationIdInterceptor;
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(correlationIdInterceptor);
+    }
 
     
 }
---
+++ src/main/java/com/wsd/edgardocswrapper/interceptors/CorrelationIdInterceptor.java
@@ -0,0 +1,31 @@
+package com.wsd.edgardocswrapper.interceptors;
+
+import com.github.f4b6a3.ulid.UlidCreator;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class CorrelationIdInterceptor implements HandlerInterceptor {
+
+    private static final String CORRELATION_ID_HEADER_NAME = "X-Correlation-Id";
+    private static final String CORRELATION_ID_LOG_VAR_NAME = "correlation_id";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String correlationId = request.getHeader(CORRELATION_ID_HEADER_NAME);
+        if (correlationId == null || correlationId.isEmpty()) {
+            correlationId = UlidCreator.getMonotonicUlid().toString();
+        }
+        MDC.put(CORRELATION_ID_LOG_VAR_NAME, correlationId);
+        response.setHeader(CORRELATION_ID_HEADER_NAME, correlationId); // Optional: Adds correlation_id back to the response header
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        MDC.remove(CORRELATION_ID_LOG_VAR_NAME);
+    }
+}
