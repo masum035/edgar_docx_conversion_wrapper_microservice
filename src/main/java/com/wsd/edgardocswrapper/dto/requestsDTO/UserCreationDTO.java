16cc37088fc1d0b09d3c89c0986685ba5c2d0797
Masum <abdullah.almasum@wsd.com>
2024-10-01 14:37:11 +0600

    User Created in Keycloak successfully

---
+++ src/main/java/com/wsd/edgardocswrapper/dto/requestsDTO/UserCreationDTO.java
@@ -0,0 +1,30 @@
+package com.wsd.edgardocswrapper.dto.requestsDTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreationDTO {
+    
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("password")
+    private String password;
+
+    @JsonProperty("email")
+    private String email;
+    
+    @JsonProperty("rolename")
+    private String roleName;
+
+    @JsonProperty("firstname")
+    private String firstName;
+
+    @JsonProperty("lastname")
+    private String lastName;
+}
